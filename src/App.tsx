af533b257bca1555ce9f0766e1888cffac3e7c5b
devmichaelheming <devmichael.heming@gmail.com>
2022-01-17 17:29:20 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import GlobalStyles from "./styles/global";
+import {
+  Main,
+  Section,
+  Sticky,
+} from "./styles/index";
+
+import FirstAndSecond from "./components/FirstAndSecond";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+
+      <Main style={{ height: '1610vh' }}>
+        <Section style={{ height: '72.7%' }}>
+          <FirstAndSecond />
+        </Section>
+
+        <Section style={{ height: '9.7%' }}>
+          <Sticky className="third" />
+        </Section>
+
+        <Section style={{ height: '10.1%' }}>
+          <Sticky className="fourth" />
+        </Section>
+      </Main>
+    </>
+  )
+}
+
+export default App
